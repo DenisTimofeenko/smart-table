90275a13b9907cb74ec30339101b1a6fcc29555d
Denis Timofeenko <rentaltech@yandex.ru>
2025-08-16 23:19:04 +0300

    first

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js"
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -7,19 +7,58 @@ import {cloneTemplate} from "../lib/utils.js";
  * @param {(action: HTMLButtonElement | undefined) => void} onAction
  * @returns {{container: Node, elements: *, render: render}}
  */
-export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+export  function initTable(settings, onAction) {
+  const { tableTemplate, rowTemplate, before, after } = settings
+  const root = cloneTemplate(tableTemplate)
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  before.reverse().forEach((subName) => {
+    // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName) // клонируем и получаем объект, сохраняем в таблице
+    root.container.prepend(root[subName].container) // добавляем к таблице после (append) или до (prepend)
+  })
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  after.forEach((subName) => {
+    // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName) // клонируем и получаем объект, сохраняем в таблице
+    root.container.append(root[subName].container) // добавляем к таблице после (append) или до (prepend)
+  })
 
-    return {...root, render};
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+
+  root.container.addEventListener("change", (e) => {
+      onAction(e.target)
+  })
+
+  root.container.addEventListener("reset", (e) => {
+    setTimeout(() => {
+      onAction()
+    }, 0)
+  })
+
+  root.container.addEventListener("submit", (e) => {
+    e.preventDefault()
+      onAction(e.submitter)
+  })
+
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate)
+      row.elements.date.textContent = item.date
+      row.elements.customer.textContent = item.customer
+      row.elements.seller.textContent = item.seller
+      row.elements.total.textContent = item.total
+      return row.container
+      Object.keys(item).forEach((key) => {
+        if (row.elements.hasOwnProperty(key)) {
+          row.elements[key].textContent = item[key]
+        }
+      })
+    })
+    root.elements.rows.replaceChildren(...nextRows)
+  }
+
+  return { ...root, render }
 }
