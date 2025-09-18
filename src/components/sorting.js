414611fa102be95f6cbcb65aa189a8405b9d6a1b
Denis Timofeenko <rentaltech@yandex.ru>
2025-09-18 23:50:46 +0300

    done

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,9 +1,10 @@
-import { sortCollection, sortMap } from "../lib/sort.js"
+import { sortMap } from "../lib/sort.js"
 
 export function initSorting(columns) {
-  return (data, state, action) => {
+  return (query, state, action) => {
     let field = null
     let order = null
+    
 
     if (action && action.name === "sort") {
       // @todo: #3.1 — запомнить выбранный режим сортировки
@@ -21,14 +22,18 @@ export function initSorting(columns) {
       })
     } else {
       // @todo: #3.3 — получить выбранный режим сортировки
-      columns.forEach(column => {                        // Перебираем все наши кнопки сортировки
-    if (column.dataset.value !== 'none') {        // Ищем ту, что находится не в начальном состоянии (предполагаем, что одна)
-        field = column.dataset.field;            // Сохраняем в переменных поле
-        order = column.dataset.value;            // и направление сортировки
-    }
-});
+      columns.forEach((column) => {
+        // Перебираем все наши кнопки сортировки
+        if (column.dataset.value !== "none") {
+          // Ищем ту, что находится не в начальном состоянии (предполагаем, что одна)
+          field = column.dataset.field // Сохраняем в переменных поле
+          order = column.dataset.value // и направление сортировки
+        }
+      })
     }
 
-    return sortCollection(data, field, order)
+    const sort = field && order !== "none" ? `${field}:${order}` : null // сохраним в переменную параметр сортировки в виде field:direction
+
+    return sort ? Object.assign({}, query, { sort }) : query // по общему
   }
 }
